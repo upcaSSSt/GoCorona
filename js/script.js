24f1ce46a717572fc3cb391d46ebc413fff6313a
harpCS <pjotrpjetya@gmail.com>
2023-05-12 10:27:11 +0300

    Add gradient scroller

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
+const scroller = document.querySelector('.scroller-gradient');
+if (scroller) {
+	window.addEventListener("scroll", function() {
+		let totalHeight = document.body.scrollHeight - window.innerHeight;
+		let scrollerHeight = window.pageYOffset / totalHeight * 100;
+		scroller.style.height = scrollerHeight + "%";
+	});
+}
+
 const headerBurger = document.querySelector('.header__burger');
 const menuBody = document.querySelector('.menu__body');
 if (headerBurger) {
-	headerBurger.addEventListener("click", function(e) {
+	headerBurger.addEventListener("click", function() {
 		document.body.classList.toggle('_lock');
 		headerBurger.classList.toggle('_active');
 		menuBody.classList.toggle('_active');
